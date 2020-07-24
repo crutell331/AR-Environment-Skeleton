2b3752adff14996ad61cb7db0320ed6ed029af6a
vincentyang01 <vincentyangcsi@gmail.com>
2020-07-24 00:25:32 -0400

    New changes

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,15 +3,15 @@ class Student < ActiveRecord::Base
     has_many :teacher, through: :student_teachers
 
 
-    # def full_name
-    #     self.first_name + " " + self.last_name
-    # end
+    def full_name
+        self.first_name + " " + self.last_name
+    end
 
 
 
-    # def self.all_in_grade(grade)
-    #     self.find_by(grade_level: grade)
-    # end
+    def self.all_in_grade(grade)
+        self.find_by(grade_level: grade)
+    end
 
 
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,13 +2,13 @@ class Teacher < ActiveRecord::Base
     has_many :student_teachers
     has_many :student, through: :student_teachers
 
-    # def tenure
-    #     if self.years_of_experience > 5
-    #         return true
-    #     else 
-    #         false
-    #     end
-    # end
+    def tenure
+        if self.years_of_experience > 5
+            return true
+        else 
+            false
+        end
+    end
 
 
 end
